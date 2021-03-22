e69f25f1fa33b211a56dfc65797baa89b666253c
RicardoMarino <ricardomarino@live.com>
2021-03-22 16:19:28 -0300

    :sparkles: Criando estrutura base para o projeto

---
+++ src/RicardoMarino.Data/Context/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RicardoMarino.Data.Context
+{
+    /// <summary>
+    /// Contexto da aplicação sem o Identity
+    /// </summary>
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base (options)
+        {
+
+        }
+    }
+}
---
+++ src/RicardoMarino.Negocio/Models/Entidade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RicardoMarino.Negocio.Models
+{
+    /// <summary>
+    /// Base para as entidades de negócio
+    /// </summary>
+    public abstract class Entidade
+    {
+        protected Entidade()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
